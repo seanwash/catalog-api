85654921aa8484770678a9782ae0890b042071b3
Sean Washington <hello@seanwash.com>
2020-04-05 23:03:14 -0700

    Remove ReleasedAt resolver
    I think this was generated as a result of a miss config

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,7 +6,6 @@ package graph
 import (
 	"context"
 	"strings"
-	"time"
 
 	"github.com/seanwash/catalog-api/graph/generated"
 	generatedmodel "github.com/seanwash/catalog-api/graph/model"
@@ -598,13 +597,3 @@ type albumResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type trackResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *albumResolver) ReleasedAt(ctx context.Context, obj *models.Album) (*time.Time, error) {
-	return &obj.ReleasedAt, nil
-}
