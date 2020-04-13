bf5aabd41dd96b8c40c9b32b9c2203f1a0640bf5
Sean Washington <hello@seanwash.com>
2020-04-13 07:03:22 -0700

    Remove redundant 'minutes' label on index track listing

--- pkg/web/pages/index.vue
+++ pkg/web/pages/index.vue
@@ -41,7 +41,6 @@
                   class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
                 >
                   {{ track.durationMs | millisecondsToMinutesAndSeconds }}
-                  minutes
                 </span>
               </div>
             </div>
